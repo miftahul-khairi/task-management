d870082b33362d8013cd22e9a2a33d56474c5c4d
Miftahul Khairi <miftahulkhairi24@gmail.com>
2025-02-20 12:20:41 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,43 +6,40 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
+// Tambahkan HasApiTokens ke Model User (app/Models/User.php)
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $timestamps = true;
+    protected $dateFormat = 'Y-m-d H:i:s';
+    protected $connection = 'mysql';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    protected $visible = [
+        'id',
+        'name',
+        'email',
+        'created_at',
+        'updated_at',
+    ];
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
