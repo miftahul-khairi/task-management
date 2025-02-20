f3f75592aa38277db272322a649da26d74e60761
Miftahul Khairi <miftahulkhairi24@gmail.com>
2025-02-20 11:45:25 +0700

    Create TaskSeeder.php

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Task;
+
+class TaskSeeder extends Seeder
+{
+    public function run()
+    {
+        Task::factory(10)->create();
+    }
+}
+
